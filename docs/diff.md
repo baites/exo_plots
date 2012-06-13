df1c94f06066e683f2961f82fae386ff9b3ce278
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2012-06-13 13:36:18 -0500

    Add diff ROOT files

---
+++ docs/diff.md
@@ -0,0 +1,34 @@
+## diff.py
+
+Compare keys in two ROOT files and report if these are different. The function
+works in similar way to Linux _diff_ tool for text files.
+
+The function scans only top level content and does not go into folders inside
+ROOT file
+
+The function returns a list with four items
+
+0. True if files are different and False otherwise
+1. keys that are found in the first file only
+2. common to both files keys
+3. keys that are found in the second file only
+
+## Example
+
+```python
+# default use
+different, lkeys, ckeys, rkeys = diff("file1.root", "file2.root")
+if different:
+    print("files are different")
+    print("left only keys:", lkeys)
+    print("right only keys:", rkeys)
+    print("common keys:", ckeys)
+```
+
+the function may also be run in verbose mode and print unique keys found in the
+first or second file if any:
+
+```python
+# verbose mode
+diff("file1.root", "file2.root", verbose=True)
+```
---
+++ root/diff.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+'''
+Created by Samvel Khalatyan, May 16, 2012
+Copyright 2012, All rights reserved
+'''
+
+from __future__ import print_function
+
+import os
+import sys
+
+import ROOT
+import root.tfile
+
+def diff(lfile, rfile, verbose=True):
+    '''
+    Compare two ROOT file keys and report if files are different
+
+    The content is compared only at the top level. No histogram equality is
+    checked.
+
+    Example:
+
+        diff("file1.root", "file2.root")
+
+    Function will return list with four items:
+
+        [0] True if files are different, False otherwise
+        [1] keys that are exclusive to file1
+        [2] common keys to two files
+        [3] keys that are found only in file2
+    '''
+
+    # Extract keys from left file
+    lkeys = set()
+    with root.tfile.topen(lfile) as lfile_:
+        # keys are cached and objects are removed once file is closed:
+        # extract the names from cache
+        #
+        lkeys = set(x.GetName() for x in lfile_.GetListOfKeys())
+
+    # Extract keys from right file
+    rkeys = set()
+    with root.tfile.topen(rfile) as rfile_:
+        rkeys = set(x.GetName() for x in rfile_.GetListOfKeys())
+
+    common_keys = lkeys & rkeys
+    lonly_keys = lkeys - common_keys
+    ronly_keys = rkeys - common_keys
+
+    if verbose and lonly_keys:
+        print("keys exlusive to file", lfile)
+        print('\n'.join("< " + str(x) for x in sorted(lonly_keys)))
+        print()
+
+    if verbose and ronly_keys:
+        print("keys exlusive to file", rfile)
+        print('\n'.join("> " + str(x) for x in sorted(ronly_keys)))
+        print()
+
+    return bool(lonly_keys or ronly_keys), lonly_keys, common_keys, ronly_keys
+
+
+
+if "__main__" == __name__:
+    try:
+        if len(sys.argv) != 3:
+            raise RuntimeError("usage: {0} file1.root file2.root".format(
+                               sys.argv[0]))
+
+        for filename in sys.argv[1:]:
+            if not os.path.exists(filename):
+                raise RuntimeError("file does not exist " + filename)
+
+        sys.exit(1 if diff(*sys.argv[1:])[0] else 0)
+
+    except RuntimeError as error:
+        print(error, file=sys.stderr)
+
+        sys.exit(1)
