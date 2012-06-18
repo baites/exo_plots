dd7fc1ccb29f51117fcd133eee002853a0c5dec8
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2012-06-18 18:55:54 -0500

    add template options documentation

---
+++ docs/template.options.md
@@ -0,0 +1,36 @@
+## tmplate.options
+
+The module defines default options that should be present in all plotting
+tools. These include:
+
+* **-v, --verbose** run application in verbose mode with debug info printed
+out
+* **-b, --batch** do not enter interactive mode and do the plotting in batch
+* **--config** use different application configuration file than
+default, e.g.: ~/.exo/template.yaml
+* **--channel-config** use cutom channel/input definition configuration file
+* **--plot-config** load user-defined plots configuration
+* **--channels** channels to be loaded
+
+The last options accepts abbreviations as described in the
+[Channel Config](https://github.com/ksamdev/exo_plots/blob/master/docs/config.channel.md#expand-channels). 
+
+## Example
+
+Consider a tool which compares the same distribution amond different channels.
+User creates a new module for the task and defines new options parser that is
+based on the default one:
+
+```python
+from template import options
+
+def parser():
+    parser_ = options.parser()
+
+    parser_.add_option(
+        "--legend-header",
+        action="store", default=None,
+        "User defined legend header on plots")
+
+    return parser_
+```
---
+++ template/options.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+'''
+Created by Samvel Khalatyan, Jun 14, 2012
+Copyright 2012, All rights reserved
+'''
+
+from optparse import OptionParser
+
+def parser():
+    '''
+    Default template options parser
+
+    This parser should be used as a baseline for all other template bases
+    analysis. For example, consider systematic analysis that uses template
+    plots but does not need scales and need to define type of the systematic
+    to be processed. Use:
+
+        def syst_parser():
+            opt_parser = parser()
+
+            opt_parser.remove_option("--scales")
+
+            opt_parser.add_option(
+                "--systematic",
+                action="store", default=None,
+                help="Systematic type to be processed")
+
+            return opt_parser
+    '''
+
+    parser_=OptionParser(usage="usage: %prog [options]")
+
+    parser_.add_option(
+            "-b", "--batch",
+            action="store_true", default=False,
+            help="Run application in batch mode: do not draw canvases")
+
+    parser_.add_option(
+            "-v", "--verbose",
+            action="store_true", default=False,
+            help="Print debug and progress info")
+
+    parser_.add_option(
+            "--config",
+            action="store", default="~/.exo/template.yaml",
+            help="template configuration")
+
+    parser_.add_option(
+            "--channel-config",
+            action="store", default=None,
+            help="input/channel templates configuration")
+
+    parser_.add_option(
+            "--plot-config",
+            action="store", default=None,
+            help="plots configuration")
+
+    parser_.add_option(
+            "--channels",
+            action="store", default=["mc", "data"]
+            help=("Load templates only for comma separated channels. Signal "
+                  "channels can be groupped with: zp, zpwide, kk. Individual "
+                  "channel can be turned OFF by prefixing it with minus, "
+                  "e.g.: zp,-zprime_m1000_w10"))
+
+    return parser_
