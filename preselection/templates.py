f777cd4d66b785f8adefb47f3889aa41d8d67987
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2012-07-17 09:21:14 -0500

    Add TFraction Fitter to the pre-selection plots

--- preselection/templates.py
+++ preselection/templates.py
@@ -8,6 +8,7 @@ Copyright 2012, All rights reserved
 from __future__ import print_function, division
 
 import math
+import sys
 
 import ROOT
 
@@ -140,3 +141,128 @@ class Cutflow(templates.Templates):
 class Templates(templates.Templates):
     def __init__(self, options, args, config):
         templates.Templates.__init__(self, options, args, config)
+
+    def load(self):
+        # Run default loading
+        templates.Templates.load(self)
+
+        # Run TFraction Fitter to get MC and QCD scales
+        try:
+            if self._verbose:
+                print("{0:-<80}".format("-- TFractionFitter "))
+
+            if ("/met" not in self.plots or
+                "/met_no_weight" not in self.plots):
+
+                raise RuntimeError("load plots /met and /met_no_weight")
+
+            # Extract met DATA, QCD and MC
+            met = {}
+            mc_channels = set(["mc", ])
+            channel.expand(self._channel_config, mc_channels)
+            for channel_, plot_ in self.plots["/met"].items():
+                if "data" == channel_:
+                    met["data"] = plot_
+                elif "qcd" == channel_:
+                    met["qcd"] = plot_
+                elif channel_ in mc_channels:
+                    if "mc" in met:
+                        met["mc"].Add(plot_)
+                    else:
+                        met["mc"] = plot_.Clone()
+
+            missing_channels = set(["data", "qcd", "mc"]) - set(met.keys())
+            if missing_channels:
+                raise RuntimeError("channels {0!r} are not loaded".format(
+                                    missing_channels))
+
+            met_noweight = None
+            for channel_, plot_ in self.plots["/met_no_weight"].items():
+                if channel_ in mc_channels:
+                    if met_noweight:
+                        met_noweight.Add(plot_)
+                    else:
+                        met_noweight = plot_.Clone()
+
+            if not met_noweight:
+                raise RuntimeError(("none of the channels {0!r} have "
+                                    "/met_no_weight").foramt(mc_channels))
+
+            # prepare MC weights for TFraction fitter
+            mc_weights = met["mc"].Clone()
+            mc_weights.Divide(met_noweight)
+
+            # Set any zero bins to at least 1 event
+            for bin_ in range(1, mc_weights.GetNbinsX() + 1):
+                if 0 >= mc_weights.GetBinContent(bin_):
+                    mc_weights.SetBinContent(bin_, 1)
+
+            # prepare variable tempaltes for TFractionFitter
+            templates_ = ROOT.TObjArray(2)
+            templates_.Add(met_noweight)
+            templates_.Add(met["qcd"])
+
+            # Setup TFractionFitter
+            fitter = ROOT.TFractionFitter(met["data"], templates_)
+            fitter.SetWeight(0, mc_weights)
+
+            # Run TFRactionFitter
+            fit_status = fitter.Fit()
+            if fit_status:
+                raise RuntimeError("fitter error {0}".format(fit_status))
+
+            # Extract MC and QCD fractions from TFractionFitter and keep
+            # only central values (drop errors)
+            fraction = ROOT.Double(0)
+            fraction_error = ROOT.Double(0)
+            fractions = {}
+
+            fitter.GetResult(0, fraction, fraction_error)
+            fractions["mc"] = float(fraction)
+
+            fitter.GetResult(1, fraction, fraction_error)
+            fractions["qcd"] = float(fraction)
+
+            # Print found fractions
+            if self._verbose:
+                print('\n'.join("{0:>3} fraction: {1:.3f}".format(key.upper(),
+                                                                  value)
+                                for key, value in fractions.items()))
+
+            # Scale all MC and QCD samples with fractions
+            for plot_, channels_ in self.plots.items():
+                # Skip normalization if one of the channels is missing
+                if ("data" not in channels_ or 
+                    "qcd" not in channels_):
+                    continue
+
+                # Get MC sum
+                mc_sum_ = None
+                for channel_, hist_ in channels_.items():
+                    if channel_ in mc_channels:
+                        if mc_sum_:
+                            mc_sum_.Add(hist_)
+                        else:
+                            mc_sum_ = hist_.Clone()
+
+                if not mc_sum_:
+                    continue
+
+                data_integral_ = channels_["data"].Integral()
+
+                mc_scale = fractions["mc"] * data_integral_ / mc_sum_.Integral()
+                for channel_, hist_ in channels_.items():
+                    if channel_ in mc_channels:
+                        hist_.Scale(mc_scale)
+                    elif "qcd" == channel_:
+                        hist_.Scale(fractions["qcd"] *
+                                    data_integral_ / hist_.Integral())
+
+        except RuntimeError as error:
+            if self._verbose:
+                print("failed to use TFractionFitter - {0}".format(error),
+                      file = sys.stderr)
+
+        finally:
+            if self._verbose:
+                print()
