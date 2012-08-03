fa17170197990cde0e68d430fbf697719b964a80
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2012-08-03 11:16:34 -0500

    Fix setup if PYTHONPATH is not set

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Created by Samvel Khalatyan, Jun 14, 2012
 # Copyright 2012, All rights reserved
 
-python -c "import sys,os; sys.exit(0 if '$PWD' in os.getenv('PYTHONPATH').split(':') else 1)"
+python -c "import sys,os; pypath = os.getenv('PYTHONPATH'); sys.exit(0 if pypath and '$PWD' in pypath.split(':') else 1)"
 if [[ 0 == $? ]]
 then
     echo "environment is set"
