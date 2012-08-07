cd6e46c161a742cf33ee70a23bd28c3a69ff2780
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2012-08-07 15:38:39 -0500

    Add DeltaR matched events plotter

--- match/templates.py
+++ match/templates.py
@@ -91,3 +91,90 @@ class Templates(templates.Templates):
 
         if signal:
             signal.Draw("9 hist same nostack c")
+
+class DRTemplates(templates.Templates):
+    ''' Produce S / B plot '''
+
+    def __init__(self, options, args, config):
+        templates.Templates.__init__(self, options, args, config)
+
+        self._background_channels = set(["ttbar",])
+
+    def plot(self):
+        '''
+        Process loaded histograms and draw them
+
+        The plot() method is responsible for processing loaded channels, e.g.
+        put data into Stack, conbine signals, etc.
+        '''
+
+        canvases = []
+        bg_channels = set(["mc", "qcd"])
+        channel.expand(self._channel_config, bg_channels)
+
+        for plot_, channels in self.plots.items():
+            # Prepare stacks for data, background and signal
+            signal = ROOT.THStack()
+            background = ROOT.THStack()
+            data = None
+            legend = ROOT.TLegend(0, 0, 0, 0) # coordinates will be adjusted
+
+            # prepare channels order and append any missing channels to the
+            # end in random order
+            order = self._channel_config["order"]
+            order.extend(set(channels.keys()) - set(order))
+
+            # process channels in order
+            backgrounds = [] # backgrounds should be added to THStack in
+                             # reverse order to match legend order
+            for channel_ in order:
+                if channel_ not in channels:
+                    continue
+
+                hist = channels[channel_]
+                hist.Scale(1 / hist.Integral())
+                hist.SetFillStyle(0)
+
+                if channel_ in bg_channels:
+                    backgrounds.append(hist)
+                    label = "l"
+
+                elif (channel_.startswith("zprime") or
+                      channel_.startswith("kkgluon")):
+                    # Signal order does not matter
+                    signal.Add(hist)
+                    label = "l"
+
+                elif channel_ == "data":
+                    data = hist
+                    label = "l"
+
+                legend.AddEntry(hist,
+                                self._channel_config["channel"][channel_]
+                                                    ["legend"],
+                                label)
+
+            # Add backgrounds to the Stack
+            if backgrounds:
+                map(background.Add, reversed(backgrounds))
+
+            canvas = self.draw_canvas(plot_,
+                                      signal=signal, background=background,
+                                      data=data, legend=legend,
+                                      uncertainty=False)
+            if canvas:
+                canvases.append(canvas)
+
+        return canvases
+
+    def draw(self, background=None, uncertainty=None, data=None, signal=None):
+        ''' Let sub-classes redefine how each template should be drawn '''
+
+        if background:
+            background.Draw("9 hist same nostack")
+
+        if data:
+            data.Draw("9 hist same")
+
+        if signal:
+            signal.Draw("9 hist same nostack")
